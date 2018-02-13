823f1a3a2a15087c8f566b4c1d83fa27ce0cb09e
rasheed-msi <rasheed.pk@cairsolutions.com>
2018-02-13 18:31:46 +0530

    changes to index and added kendo package

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 
+import '@progress/kendo-ui'
+// import '@progress/kendo-theme-default/dist/all.css'
+// import { Calendar } from '@progress/kendo-dateinputs-vue-wrapper'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

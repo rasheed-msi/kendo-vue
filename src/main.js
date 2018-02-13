6bed7e245f3a9cc45f23f72c49b8a3853500d9d8
rasheed-msi <rasheed.pk@cairsolutions.com>
2018-02-13 20:00:14 +0530

    Added grid and data source package to vue from kendo

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import App from './App'
 
 import '@progress/kendo-ui'
 import { KendoButtonGroup, KendoButtonsInstaller } from '@progress/kendo-buttons-vue-wrapper'
+import { KendoGridColumn, KendoGrid, KendoGridInstaller } from '@progress/kendo-grid-vue-wrapper'
+import { KendoDataSource, KendoDataSourceInstaller } from '@progress/kendo-datasource-vue-wrapper'
 
 Vue.config.productionTip = false
 Vue.use(KendoButtonsInstaller)
+Vue.use(KendoGridInstaller)
+Vue.use(KendoDataSourceInstaller)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App, KendoButtonGroup }
+  components: { App, KendoGrid, KendoGridColumn, KendoButtonGroup, KendoDataSource }
 })
